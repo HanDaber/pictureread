5769f385bb37e54387ee0a2e907badc11ac4ae17
Dan Haber <handaber@gmail.com>
2013-03-22 12:39:43 -0700

    --amend

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,22 @@
+var info_form = $('#info_form'),
+    submit_info = info_form.find('a')
+    email_pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/),
+    frame_links_resize = $('.frames').find('a'),
+    story_image_resize = $('#story');
+
+var frame = {
+    objects: [
+        { icon: '', text: 'Hello, I am an object!', position: { x: 1, y: 10 } },
+        { icon: '', text: 'Hello, I am a cool thing!', position: { x: 25, y: 33 } }
+    ]
+};
+
+var objs = frame.objects,
+    frameimage = $('#frame_image');
+
+
+
+
 // Initialize:
 Handlebars.registerHelper("each_i", function ( array, fn ) {
 
@@ -19,13 +38,9 @@ Handlebars.registerHelper("each_i", function ( array, fn ) {
  
 });
 
+
+
 // Splash Page:
-
-var info_form = $('#info_form'),
-	submit_info = info_form.find('a');
-
-var email_pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/);
-
 info_form.submit(function ( ev ) {
 
     var $this = $(this);
@@ -133,15 +148,7 @@ $('.destroy_story').on('click', function ( event ) {
 
 
 // Frame:
-var frame = {
-    objects: [
-        { icon: '', text: 'Hello, I am an object!', position: { x: 1, y: 10 } },
-        { icon: '', text: 'Hello, I am a cool thing!', position: { x: 25, y: 33 } }
-    ]
-};
 
-var objs = frame.objects,
-    frameimage = $('#frame_image');
 
 for( var i = 0, o = objs.length; i < o; i++ ) {
 
@@ -220,18 +227,12 @@ $('.edit_form').on('submit', function ( event ) {
     });
 });
 
-var frame_links_resize = $('.frames').find('a'),
-    story_image_resize = $('#story');
+
 
 // WINDOW.RESIZE
 $(window).resize(function () {
 
     element_height( frame_links_resize );
-    
-    // element_height( $('.file_picker') );
-
-    // story_background_image( story_image_resize );
-
 });
 
 element_height( frame_links_resize );
@@ -339,135 +340,6 @@ function story_background_image ( s ) {
 
         console.log(frame.width())
 
-    frame.append('<img src="' + image + '">');   //.css('background', 'url(\'' + image + '\')'); //.css('background-size', '\'' + frame.width() + ' ' + frame.height() + '\'');
+    frame.append('<img src="' + image + '">');
 
-    // if( typeof( frame.position() ) !== 'undefined' ) {
-
-    //     p = frame.position().top;
-    //     w = $(window).height();
-
-    //     frame.height(function () {
-    //         return w - p;
-    //     });
-    // }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function initialize() {
-//     var mapOptions = {
-//       zoom: 4,
-//       disableDefaultUI: true,
-//       center: new google.maps.LatLng(-33, 151),
-//       mapTypeId: google.maps.MapTypeId.ROADMAP
-//     }
-//     var map = new google.maps.Map(document.getElementById('frame'),
-//                                   mapOptions);
-
-//     var image = '/img/loader.gif';
-//     var myLatLng = new google.maps.LatLng(-33.890542, 151.274856);
-//     var beachMarker = new google.maps.Marker({
-//         position: myLatLng,
-//         map: map,
-//         icon: image
-//     });
-// }
-
-
-
-
-
-// initialize();
-
-
-
-
-
-// var moonTypeOptions = {
-    
-//     getTileUrl: function(coord, zoom) {
-        
-//         var normalizedCoord = getNormalizedCoord(coord, zoom);
-        
-//         if (!normalizedCoord) return null;
-
-//         var bound = Math.pow(2, zoom);
-        
-//         return 'http://localhost:3000/img/characters.png';
-//     },
-
-//     tileSize: new google.maps.Size(256, 256),
-//     maxZoom: 0,
-//     minZoom: 0,
-//     radius: 1738000,
-//     name: 'Moon'
-// };
-
-// var moonMapType = new google.maps.ImageMapType(moonTypeOptions);
-
-// function initialize() {
-
-//     var myLatlng = new google.maps.LatLng(0, 0);
-
-//     var mapOptions = {
-//         center: myLatlng,
-//         zoom: 1,
-           // disableDefaultUI: true,
-//         streetViewControl: false,
-//         mapTypeControlOptions: {
-//             mapTypeIds: ['moon']
-//         }
-//     };
-
-//     var map = new google.maps.Map(document.getElementById('frame'), mapOptions);
-
-//     map.mapTypes.set('moon', moonMapType);
-    
-//     map.setMapTypeId('moon');
-// }
-
-// // Normalizes the coords that tiles repeat across the x axis (horizontally)
-// // like the standard Google map tiles.
-// function getNormalizedCoord(coord, zoom) {
-    
-//     var y = coord.y;
-    
-//     var x = coord.x;
-
-//     // tile range in one direction range is dependent on zoom level
-//     // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
-//     var tileRange = 1 << zoom;
-
-//     // don't repeat across y-axis (vertically)
-//     if (y < 0 || y >= tileRange) {
-//         return null;
-//     }
-
-//     // repeat across x-axis
-//     if (x < 0 || x >= tileRange) {
-//         x = (x % tileRange + tileRange) % tileRange;
-//     }
-
-//     return {
-//         x: x,
-//         y: y
-//     };
-// }
-
-// initialize();
-
-
